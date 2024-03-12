68ae7a1cf95481079c61f1b456633b2540018a8c
Geethesh Nair <geetheshnair123@gmail.com>
2024-03-12 22:50:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont:["Montserrat, sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
+
